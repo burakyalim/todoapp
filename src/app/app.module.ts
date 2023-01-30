4815c06397d50764f457ba2f11168c5d1a62e6a8
burakyalimm07 <121548646+burakyalimm07@users.noreply.github.com>
2023-01-30 21:43:00 +0300

    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { AppComponent } from './app.component';
+import { TodoComponent } from './todo/todo.component';
+import { ListComponent } from './todo/list/list.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TodoComponent,
+    ListComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
